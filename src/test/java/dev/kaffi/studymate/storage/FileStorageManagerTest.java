54408f151477e8741d7b78d79a624e9b8408b08c
Filip Kafka <filip.kafka@gmail.com>
2026-09-01 09:37:35 +0200

    add FileStorageManager with StorageManager interface and StorageException implementation

---
+++ src/test/java/dev/kaffi/studymate/storage/FileStorageManagerTest.java
@@ -0,0 +1,11 @@
+package dev.kaffi.studymate.storage;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FileStorageManagerTest {
+}
