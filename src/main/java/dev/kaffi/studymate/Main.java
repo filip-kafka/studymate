c1d72277f53a243a5d41b72a136668601166eee4
Filip Kafka <filip.kafka@gmail.com>
2026-09-11 23:20:06 +0200

    main updates

--- src/main/java/dev/kaffi/studymate/Main.java
+++ src/main/java/dev/kaffi/studymate/Main.java
@@ -4,7 +4,10 @@ import dev.kaffi.studymate.cli.Dispatcher;
 import dev.kaffi.studymate.cli.Formatter;
 import dev.kaffi.studymate.cli.Result;
 import dev.kaffi.studymate.domain.SessionService;
+import dev.kaffi.studymate.domain.StorageManager;
+import dev.kaffi.studymate.domain.StudyMateException;
 import dev.kaffi.studymate.storage.FileStorageManager;
+import jdk.internal.org.commonmark.internal.inline.AsteriskDelimiterProcessor;
 
 import java.nio.file.Path;
 import java.time.Clock;
@@ -12,29 +15,40 @@ import java.time.Duration;
 
 public class Main {
 
-	final static Formatter formatter = new Formatter();
-	final static SessionService sessionService = new SessionService(
-			Clock.tick(Clock.systemUTC(), Duration.ofSeconds(1)),
-			new FileStorageManager(Path.of(System.getProperty("user.home")).resolve(".studymate"))
-	);
-	final static Dispatcher dispatcher = new Dispatcher(sessionService, formatter);
+	public static void main(String[] args) {
 
-	static void main(String[] args) {
-		Result dispatchResult = dispatcher.dispatch(args);
+	    String baseDir = System.getenv("STUDYMATE_HOME");
+	    Path path = Path.of(baseDir != null ? baseDir : System.getProperty("user.home")).resolve(".studymate");
 
-		switch (dispatchResult.outcome()) {
-			case SUCCESS -> {
-				System.out.println(dispatchResult.content());
-				System.exit(0);
+	    StorageManager storageManager = new FileStorageManager(path);
+
+	    Formatter formatter = new Formatter();
+		SessionService sessionService = new SessionService(Clock.tick(Clock.systemDefaultZone(), Duration.ofSeconds(1)), storageManager);
+		Dispatcher dispatcher = new Dispatcher(sessionService, formatter);
+
+		try {
+			Result dispatchResult = dispatcher.dispatch(args);
+
+			switch (dispatchResult.outcome()) {
+				case SUCCESS -> {
+					System.out.println(dispatchResult.content());
+					System.exit(0);
+				}
+				case USER_ERROR -> {
+					System.err.println(dispatchResult.content());
+					System.exit(1);
+				}
+				case SYSTEM_ERROR -> {
+					System.err.println(dispatchResult.content());
+					System.exit(2);
+				}
 			}
-			case USER_ERROR -> {
-				System.err.println(dispatchResult.content());
-				System.exit(1);
-			}
-			case SYSTEM_ERROR -> {
-				System.err.println(dispatchResult.content());
-				System.exit(2);
-			}
+		} catch (StudyMateException e) {
+			System.err.println(e.getMessage());
+			System.exit(2);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			System.exit(2);
 		}
 	}
 }
